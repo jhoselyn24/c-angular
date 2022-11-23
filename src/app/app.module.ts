ddf6332c7936c7847ba871e40796eb5ccf70678e
Nikki <inga.jhoselyn@gmail.com>
2022-11-23 16:30:52 -0500

    componentes-inga
    Primer Desafio, agregando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './desafio1/toolbar/toolbar.component';
+import { NavbarComponent } from './desafio1/navbar/navbar.component';
+import { StudentsComponent } from './desafio1/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule
